0f8b657031fa91d54b5fc7f9397090d8bc8a3afa
Ed Wantuil <ed@wantuil.com>
2024-03-15 23:14:53 -0300

    ci: adiciona github actions

--- rds.tf
+++ rds.tf
@@ -1,13 +1,16 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#
-#resource "aws_rds_cluster" "aurora_postgres" {
-#  cluster_identifier  = "aurora-postgres-cluster"
-#  engine              = "aurora-postgresql"
-#  engine_version      = "12.4"
-#  database_name       = "tech-challenge"
-#  master_username     = "var.user_name"
-#  master_password     = "var.password"
-#  skip_final_snapshot = true
-#}
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::058264138215:role/LabRole"
+  }
+}
+
+resource "aws_rds_cluster" "aurora_postgres" {
+  cluster_identifier  = "aurora-postgres-cluster"
+  engine              = "aurora-postgresql"
+  engine_version      = "12.4"
+  database_name       = "tech-challenge"
+  master_username     = "var.user_name"
+  master_password     = "var.password"
+  skip_final_snapshot = true
+}
