c7fd0e93ac9589c5693867c63aea94be5a692216
Ed Wantuil <ed@wantuil.com>
2024-03-16 11:23:53 -0300

    ci: adiciona github actions

--- rds.tf
+++ rds.tf
@@ -7,8 +7,8 @@ resource "aws_rds_cluster" "aurora_postgres" {
   engine              = "aurora-postgresql"
   engine_version      = "15.4"
   database_name       = "techchallenge"
-  master_username     = "techchallenge"
-  master_password     = "techchallenge"
+  master_username     = var.master_username
+  master_password     = var.master_password
   skip_final_snapshot = true
 }
 
