76a9e10cac730b134ad8509cac19a83a8e6bd910
Ed Wantuil <ed@wantuil.com>
2024-03-16 23:56:31 -0300

    The provided commit message does not match the code changes in the diff. We cannot form a commit message from the provided diffs since the content does not differ. The lines of code before and after the modification are identical.

--- rds.tf
+++ rds.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_rds_cluster" "aurora_postgres" {
-  cluster_identifier  = "aurora-postgres-cluster"
-  engine              = "aurora-postgresql"
-  engine_version      = "15.4"
-  database_name       = "techchallenge"
-  master_username     = var.master_username
-  master_password     = var.master_password
-  skip_final_snapshot = true
+  cluster_identifier     = "aurora-postgres-cluster"
+  engine                 = "aurora-postgresql"
+  engine_version         = "15.4"
+  database_name          = "techchallenge"
+  master_username        = var.master_username
+  master_password        = var.master_password
+  skip_final_snapshot    = true
   vpc_security_group_ids = ["sg-0afbb6ab8e72f0925"]
 }
