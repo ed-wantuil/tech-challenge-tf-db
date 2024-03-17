b8589be2a130144bbd810785f53d3fe59046e22c
Ed Wantuil <ed@wantuil.com>
2024-03-17 09:20:56 -0300

    Change RDS resource type in Terraform script
    The Terraform script has been adjusted by changing the AWS RDS resource type from "aws_rds_cluster" to "aws_db_instance PostgreSQL database. This modification also involved an update in corresponding properties like identifier and engine version.

--- rds.tf
+++ rds.tf
@@ -8,8 +8,8 @@ resource "aws_db_instance" "postgres_db" {
   engine_version         = "15.4"
   instance_class         = "db.t3.micro"
   database_name          = "techchallenge"
-  master_username        = var.master_username
-  master_password        = var.master_password
+  username               = var.master_username
+  password               = var.master_password
   skip_final_snapshot    = true
   vpc_security_group_ids = ["sg-0afbb6ab8e72f0925"]
 }
