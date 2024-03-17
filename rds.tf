35fe84da197784ecb204e4fd27445e79019d2f20
Ed Wantuil <ed@wantuil.com>
2024-03-17 09:30:08 -0300

    Add allocated storage to RDS instance
    The Terraform configuration for the AWS RDS instance has been updated to include storage allocation. An "allocated_storage" property has been added and set to 20, determining the storage size for the database instance. This modification enhances the control over instance resource allocation.

--- rds.tf
+++ rds.tf
@@ -7,6 +7,7 @@ resource "aws_db_instance" "postgres_db" {
   engine                 = "postgres"
   engine_version         = "15.4"
   instance_class         = "db.t3.micro"
+  allocated_storage      = 20
   username               = var.master_username
   password               = var.master_password
   skip_final_snapshot    = true
